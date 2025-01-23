3748e40687679aebeba2d98a4f5aac22e10d989f
sachin hedaoo <sachinhd517@gmail.com>
2025-01-23 15:30:01 +0530

    we implement carousel home page

--- RestaurantBoolingApp.UI/src/app/app.module.ts
+++ RestaurantBoolingApp.UI/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core';
+import { importProvidersFrom, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './compoents/core/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/core/nav-bar/nav-bar.component';
+import { CarouselComponent, CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
-  providers: [],
+  providers: [
+    importProvidersFrom(CarouselModule.forRoot())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
